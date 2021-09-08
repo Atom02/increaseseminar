036da8e7c4224b7f837dae551975b9e81aa1debe
Atom03 <candra.nurihsan9@gmail.com>
2021-09-08 17:13:15 +0700

    Layout fixes

--- FRONTEND/src/pages/Index.vue
+++ FRONTEND/src/pages/Index.vue
@@ -86,7 +86,7 @@
             <span class="text-h6 text-weight-regular">
               <i>
                 <s
-                  >Hybrid Meeting online and onsite in Padang, 20-22 September
+                  >Hybrid Meeting online and onsite in Padang, 20-21 September
                   2021</s
                 > </i
               ><br />
